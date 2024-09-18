695792ead4e711c4d0fc3922f7c6439407baed2c
yurlov <daze29@mail.ru>
2024-09-18 15:46:51 +0300

    DZ3 AbsTable1

--- src/main/java/tables/AbsTable.java
+++ src/main/java/tables/AbsTable.java
@@ -4,6 +4,7 @@ import db.IDBConnect;
 import db.MySQLConnect;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public abstract class AbsTable implements ITable{
@@ -18,8 +19,10 @@ public abstract class AbsTable implements ITable{
     //CREATE TABLE tableName (column1 INT PRIMARY KEY , column2 VARCHAR(255), column3, ...);
     @Override
     public void create(List<String> columns) {
-        delete();
-        dbConnector.execute(String.format("CREATE TABLE %s (%s);", tableName, String.join(",", columns)));
+        // Проверяем существование таблицы
+        if (!isTableExists()) {
+            dbConnector.execute(String.format("CREATE TABLE %s (%s);", tableName, String.join(",", columns)));
+        }
     }
 
     @Override
@@ -27,8 +30,19 @@ public abstract class AbsTable implements ITable{
         dbConnector.execute(String.format("drop table if exists %s;",this.tableName));
     }
 
-    public ResultSet selectall() {
+    public ResultSet selectAll() {
         return dbConnector.executeQuery(String.format("SELECT * FROM  %s;",this.tableName));
+    }
+
+    // Метод для проверки существования таблицы
+    private boolean isTableExists() {
+        String query = String.format("SHOW TABLES LIKE '%s';", this.tableName);
+        try (ResultSet rs = dbConnector.executeQuery(query)) {
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 }
